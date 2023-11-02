c35d59bd3a4108f2060abd4ddec0da06d68106b5
Krisluvoh <krisluvohio@gmail.com>
2023-11-02 19:45:05 -0400

    project-commit

--- script.js
+++ script.js
@@ -11,20 +11,110 @@ for (let i=0;i<moneyArray.length;i++){
 console.log(playerArray);
 return playerArray;
 }
+//Function to calculate the score
 
-const testArray=makePlayerArray();
+function calculateScore(playerArray) {
+    let score = 0;
+    for (let i = 0;i<
+        playerArray.length; i++){
+            score+=playerArray[i];
+        }
+return score;
+    }
+
+    //Function to check for a win
+
+    function checkWin(playerArray){
+        const targetScore=100;
+        const score=
+        calculateScore(playerArray);
+        if (score>=targetScore){
+            return true;
+        }
+return false;
+    }
+
+    //Function to reset the game
+
+    function resetGame(){
+        const playerArray= makePlayerArray();
+        textOutput.textContent='Game reset. Start a new game!';
+        return playerArray;
+    }
+
+    const testArray=makePlayerArray();
 console.log(testArray);
 
 
-const button=document.createElement('button');
+const startButton=document.getElementById('startButton');
+//const keepGoingButton=document.getElementById('keepGoingButton');
+//const stopButton=document.getElementById('stopButton');
+const textOutput=document.getElementById('textOutput');
 
-button.setAttribute('id','myButton');
-button.textContent='Start','Stop', 'Quit';
+const score=calculateScore(playerArray);
+textOutput.textContent='Current score: $${score}';
+
+const isWin=checkWin(playerArray);
+if (isWin){
+    textOutput.textContent='Congratulations!You are the winner! You are now a DOLLARNAIRE!!!';
+}
 
 
-const parentElement=document.getElementById('parentElement');
 
-parentElement.appendChild(button);
+
+startButton.addEventListener('click',()=>
+{
+    const playerArray=makePlayerArray();
+    textOutput.textContent='Game started. Good luck!';
+});
+
+
+
+//keepGoingButton.addEventListener('click',()=>
+
+   // textOutput.textContent='Keep going button clicked';
+//});
+
+//stopButton.addEventListener('click',()=>
+//{
+  //  textOutput.textContent='Stop button clicked';
+//});
+
+const quitButton=document.getElementById('quitButton');
+quitButton.addEventListener('click',()=>
+{
+    const playerArray=[];
+    const score=
+    calculateScore(playerArray);
+    textOutput.textContent='Final score: $${score}';
+})
+
+    const confirmation=confirm('Are you sure you want to quit?');
+    if(confirmation){
+        textOutput.textContent="Quit button clicked. Quitting the game...";
+    }else{
+        textOutput.textContent="Quit button clicked. Resuming the game...";
+    }
+
+
+const resetButton=document.getElementById('resetButton');
+resetButton.addEventListener('click',()=>
+{
+const playerArray=resetGame();
+});
+
+
+
+    textOutput.textContent="Reset button clicked";
+
+
+//button.setAttribute('id','myButton');
+//button.textContent='Start','Stop', 'Quit';
+
+
+//const parentElement=document.getElementById('parentElement');
+
+//parentElement.appendChild(button);
 
 
 class Player{
